5792fe94a6254e2d42af49fec3ea367ea951bdd6
Wellerson <viana.wellerson@gmail.com>
2018-09-07 19:52:13 -0300

    main file

--- normalizacao-histograma/main.py
+++ normalizacao-histograma/main.py
@@ -1,13 +1,22 @@
 import numpy as np
+from matplotlib import pyplot as plt
 import cv2 as cv
 
 image = cv.imread("rapha.png")
 image = cv.resize(src = image, dst = None, dsize = (0, 0), fx = 0.5, fy = 0.5, interpolation = cv.INTER_AREA)
 
-#hist_image = cv.calcHist([image], [0], None, [256], [0,256])
-
 cv.imshow("Imagem", image)
 cv.waitKey()
+
+plt.subplot(131)
+plt.hist(image[:, :, 0].ravel(),256,[0,256], color = 'b');
+plt.subplot(132)
+plt.hist(image[:, :, 1].ravel(),256,[0,256], color = 'g');
+plt.subplot(133)
+plt.hist(image[:, :, 2].ravel(),256,[0,256], color = 'r');
+
+plt.show()
+
 
 # normalizing
 new_image = image.copy()
@@ -15,3 +24,12 @@ new_image = image.copy()
 new_image = cv.normalize(image, new_image, 0, 255, cv.NORM_MINMAX)
 cv.imshow("Imagem 2", new_image)
 cv.waitKey()
+
+plt.subplot(131)
+plt.hist(new_image[:, :, 0].ravel(),256,[0,256], color = 'b');
+plt.subplot(132)
+plt.hist(new_image[:, :, 1].ravel(),256,[0,256], color = 'g');
+plt.subplot(133)
+plt.hist(new_image[:, :, 2].ravel(),256,[0,256], color = 'r');
+
+plt.show()
